3e2d5df87a57cc7eb55b7db923ad1c841523e3bb
Xeuten <xeuten@gmail.com>
2024-12-12 00:05:14 +0200

    fixed entrypoint for deploying

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, send_from_directory
+from flask import Blueprint, request
 from flask_swagger_ui import get_swaggerui_blueprint
 from google.cloud import datastore
 
